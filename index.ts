cd2bbb08a72dcce2431adbcdd82e2d60c7299dc8
Vikas Chouhan <vikaschouhan360@gmail.com>
2023-12-10 13:12:05 +0530

    Check

--- index.ts
+++ index.ts
@@ -40,6 +40,9 @@ app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
+app.get("/", (req, res) => {
+    res.status(200).json("Hii")
+})
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
